35be4d4ff1fbc236a8a09a761d7285435bee19c2
unknown <ziang6@ualberta.ca>
2024-02-14 12:52:40 -0700

    finish adjust lighting

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,11 +27,10 @@ const Home = () => {
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
-          <directionalLight />
-          <ambientLight />
-          <pointLight />
-          <spotLight />
-          <hemisphereLight />
+          {/* TODO: maybe adjust the light position according to the time */}
+          <directionalLight position={[3,1,1]} intensity={2}/> 
+          <ambientLight intensity={0.5}/>
+          <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
           <Island 
             position={islandPosition}
             scale={islandScale}
