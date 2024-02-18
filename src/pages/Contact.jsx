8d3a1c8f271ceeff3e09261b77c39c13e6e0d9be
unknown <ziang6@ualberta.ca>
2024-02-17 19:53:16 -0700

    base contact form done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
 
 const Contact = () => {
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+  const [isLoading, setIsLoading] = useState(false);
+  const formRef = useRef(null);
+  const handleChange = () => {};
+  const handleFocus = () => {};
+  const handleBlur = () => {};
+  const handleSubmit =() => {};
   return (
-    <div>Contact</div>
-  )
-}
+    <section className="relative flex lg:flex-row flex-col max-container">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get in touch</h1>
+        <form action="" className="w-full flex flex-col gap-7 mt-14" onSubmit={handleSubmit}>
+          <label className="text-black-500 font-semibold">
+            Name
+            <input
+              type="text"
+              name="name"
+              className="input"
+              placeholder="Your name"
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Email
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="your@email.com"
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Your message
+            <textarea
+              name="message"
+              className="textarea"
+              placeholder="Leave me a message !"
+              rows={5}
+              required
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <button
+            type="submit"
+            className="btn"
+            disabled={isLoading}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          >
+           {isLoading ? 'Sending...' : "Send message"} 
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
