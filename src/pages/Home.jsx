ea6e065f743f83b776592439a88822c755605aa2
unknown <ziang6@ualberta.ca>
2024-03-13 01:35:09 -0600

    Finish adding sound and logo
    Change the page name and icon of the tab.
    Add music to main page.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,59 +1,81 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
-import { Suspense, useState } from "react";
+import { Suspense, useState, useEffect, useRef } from "react";
 import Loader from "../components/Loader";
 import Island from "../models/Island";
 import Sky from "../models/Sky";
 import Bird from "../models/Bird";
 import Plane from "../models/Plane";
 import HomeInfo from "../components/HomeInfo";
+import hi from "../assets/hi.mp3";
+import { flask, soundoff, soundon } from "../assets/icons";
 const Home = () => {
+  const audioRef = useRef(new Audio(hi));
+  audioRef.current.volume = 0.4;
+  audioRef.current.loop = true;
   const [isRotating, setIsRotating] = useState(false);
   const [currentStage, setCurrentStage] = useState(1);
+  const [isPlayingMusic, setIsPlayingMusic] = useState(false);
+  useEffect(() => {
+    if (isPlayingMusic) {
+      audioRef.current.play();
+    }
+    return () => {
+      audioRef.current.pause();
+    }
+  }, [isPlayingMusic]);
   // function to adjust island for different devices
-  const adjustIslandForScreenSize = () =>{
+  const adjustIslandForScreenSize = () => {
     let screenScale = null;
-    let screenPosition =[0, -6.5, -43];
+    let screenPosition = [0, -6.5, -43];
     let rotation = [0.1, 4.7, 0];
-    if(window.innerWidth<768){
+    if (window.innerWidth < 768) {
       screenScale = [0.9, 0.9, 0.9];
     } else {
       screenScale = [1, 1, 1];
     }
     return [screenScale, screenPosition, rotation];
-  }
+  };
 
   // adjust plane for sceen size
-  const adjustPlaneForScreenSize = () =>{
-    let planeScale, planePosition = null;
-    if(window.innerWidth<768){
+  const adjustPlaneForScreenSize = () => {
+    let planeScale,
+      planePosition = null;
+    if (window.innerWidth < 768) {
       planeScale = [1.5, 1.5, 1.5];
-      planePosition = [0.-1.5,0];
+      planePosition = [0 - 1.5, 0];
     } else {
       planeScale = [3, 3, 3];
       planePosition = [0, -4, -4];
     }
     return [planeScale, planePosition];
-  }
-  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+  };
+  const [islandScale, islandPosition, islandRotation] =
+    adjustIslandForScreenSize();
   const [planeScale, planePosition] = adjustPlaneForScreenSize();
   return (
     <section className="w-full h-screen relative">
       <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
-        {currentStage && <HomeInfo currentStage={currentStage} /> }
+        {currentStage && <HomeInfo currentStage={currentStage} />}
       </div>
       <Canvas
-        className={`w-full h-screen bg-transparent ${isRotating? 'cursor-grabbing' : 'cursor-grab'}`}
+        className={`w-full h-screen bg-transparent ${
+          isRotating ? "cursor-grabbing" : "cursor-grab"
+        }`}
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
           {/* TODO: maybe adjust the light position according to the time */}
-          <directionalLight position={[3,1,1]} intensity={2}/> 
-          <ambientLight intensity={0.5}/>
-          <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
+          <directionalLight position={[3, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          <hemisphereLight
+            skyColor="#b1e1ff"
+            groundColor="#000000"
+            intensity={1}
+          />
           <Bird />
           <Sky isRotating={isRotating} />
-          <Island 
+          <Island
             position={islandPosition}
             scale={islandScale}
             rotation={islandRotation}
@@ -61,14 +83,22 @@ const Home = () => {
             setIsRotating={setIsRotating}
             setCurrentStage={setCurrentStage}
           />
-          <Plane 
-            isRotating = {isRotating}
-            scale = {planeScale}
-            position = {planePosition}
-            rotation = {[0,20,0]}
+          <Plane
+            isRotating={isRotating}
+            scale={planeScale}
+            position={planePosition}
+            rotation={[0, 20, 0]}
           />
         </Suspense>
       </Canvas>
+      <div className="absolute bottom-2 left-2">
+        <img
+          src={!isPlayingMusic ? soundoff : soundon}
+          alt="sound"
+          className="w-10 h-10 cursor-pointer object-contain"
+          onClick={() => setIsPlayingMusic(!isPlayingMusic)}
+        />
+      </div>
     </section>
   );
 };
